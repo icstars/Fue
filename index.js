c701c9251216d101d68644df05e41877b1e29028
Fvue95 <136373045+Fvue95@users.noreply.github.com>
2023-06-21 19:34:01 -0500

    Edited Assignment
    6/21

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ let Age = prompt(`How old are you`);
 let Hobby = prompt(`What is your hobby?`);
 
 console.log(`How old are you`);
-alert(`Hi my name is${Name}, I am ${Age}Years old and I like${Hobby}`);
+alert(`Hi my name is ${Name}, I am ${Age} Years old and I like ${Hobby}`);
