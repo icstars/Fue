0585885c10e4056b003753b366a36af38acbf336
Fvue95 <136373045+Fvue95@users.noreply.github.com>
2023-06-21 19:30:28 -0500

    js
    javascript prompt

---
+++ index.js
@@ -0,0 +1,8 @@
+let Name = prompt(`What is your name?`);
+
+let Age = prompt(`How old are you`);
+
+let Hobby = prompt(`What is your hobby?`);
+
+console.log(`How old are you`);
+alert(`Hi my name is${Name}, I am ${Age}Years old and I like${Hobby}`);
