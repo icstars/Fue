bf0418f18ea2d272f50bf7e3ec74d789c543350e
Fvue95 <136373045+Fvue95@users.noreply.github.com>
2023-06-26 19:23:51 -0500

    Collin's challenge

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // let name = prompt(`what is your name?`);
 
+// console.log is for you as a developer to see
+
 // let age = prompt(`how old are you`);
 
 // let hobby = prompt(`what is your hobby?`);
@@ -114,11 +116,157 @@
 // If the user's guess matches the generated number, an alert is shown with a success message.
 // If the user's guess is incorrect, an alert is shown with a failure message.
 
-let randomNumber = Math.ceil(Math.random() * 10);
-console.log(randomNumber);
-let prompt1 = prompt("pick a number between 1-10");
-if (prompt1 == randomNumber) {
-  alert("You got it right");
-} else {
-  alert("Please leave you are wrong");
+// let randomNumber = Math.ceil(Math.random() * 10);
+// console.log(randomNumber);
+// let prompt1 = prompt("pick a number between 1-10");
+// if (prompt1 == randomNumber) {
+//  alert("You got it right");
+// } else {
+//  alert("Please leave you are wrong");
+// }
+
+// for
+
+// for(let i = 0; i<10; i==) {
+// console.log(i);
+// }
+
+// As long i is 10 its gonna keep looping
+
+// arrays and methods
+
+// let team= ["lucycan", "collin","Mark", "Cortez"]
+// console.log(team);
+// console.log(team.length);
+
+/// push
+
+// console.log(team.push("derrck", "john"))
+
+/// Pop
+
+//console.log(team.pop());
+
+//console.log();
+
+//Split
+// let abc = "a,b,c"; //string
+// let splitArr = abc.split(","); //created an array splitArr and assigned it the values from abc string
+// console.log("splitArr:", splitArr);
+
+// declare a variable use: let , const , or var
+// let team;
+// assign an array to a variable.
+// team = ["Lucycan", "Collin", "Mark", "Cortez", "Fue"];
+// create a new array roles
+// let roles = [
+//  "Tech Manager",
+//  "Techfello",
+//  "Placement Manager",
+//  "Program Manager",
+//  "Intern",
+// ];
+
+//syntax for a: for loop  for(let i = 0; i < X; i++) {code block}
+
+// write a loop that will display the names in the team array and their job role
+// ` ${}` temperate literal syntax
+//for (let i = 0; i < team.length; i++) {
+//  console.log("i:", i);
+//  console.log(`Hello, ${team[i]}, job: ${roles[i]}`);
+// }
+
+// Take in a string value from a user that will be a list of their team names;
+//list = array. user = prompt or form,
+//take in a value from a user:
+//let team2 = prompt("What are your team member names?");
+//convert the user string to an array
+//let teamarr = team2.split(",");
+//console.log(teamarr);
+// take in the user roles on their team
+//let teamroles = prompt("what are your team members roles?");
+//let teamposition = teamroles.split(",");
+//console.log(teamposition);
+//display the team member name and role for all of their members
+// for (let i = 0; i < team.length; i++) {
+//  console.log("i:", i);
+//  console.log(`Hello, ${team[i]}, job: ${roles[i]}`);
+// }
+
+//REVIEW -
+// initializer
+// condition
+// iterator
+
+// let array = [1,2,3,4,5]
+
+// for(let initializer = 0; initializer < array.length; initializer++) {
+// console.log(array[initializer])
+// }
+
+//SECTION - FUNCTIONS
+
+// SYNTAX FOR FUNCTION
+
+// function thisIsTheFunctionName(name) {
+//  console.log(name)
+// }
+
+// TYPE OF FUNCTION NAME FOLLOWED BY PARENTHESIS
+// thisIsTheFunctionName('Fue') // In the parentehsis is the argument
+
+// iterateThroughArray(array3)
+
+// RETURN STATEMENT
+
+// const name = thisIsTheFunctionName('vince')
+
+// function iterateThroughArray(arr){
+//  for(let initializer = 0; initializer < arr.length; initializer++) {
+//    console.log(arr[initializer])
+//  }
+// }
+
+//iteratrThoughArray(array3)
+
+//function sum(num1, num2) {
+//  console.log(num1 + num2)
+// }
+
+// sum(1,3)
+
+// const function1 = function() {
+
+// }
+
+// Arrow Function
+// const funtion 2 = () => {
+
+// }
+
+// let array10 = [1,2,3,4,5]
+
+// const function21 = function(arr) {
+//  for(let initializer = 0; initializer < arr.length; initializer++) {
+//    console.log(arr[initializer])
+//  }
+// }
+
+// function21(array10)
+
+// loop()
+
+function blockScope(Name) {
+  let team = ["Lucycan", "Collin", "Mark", "Cortez"];
+  team.push(Name);
+  console.log(team); // You can't access in GLOBAL SCOPE
 }
+let blanca = "Blanca";
+blockScope(blanca);
+
+///CHALLENGE
+let team = ["Lucycan", "Collin", "Mark", "Cortez"]; ///this array needs to be in block scope not global scope
+///create a function that passes another name as an argument to the function parameter
+/// the function adds the name that you passed to the array
+/// return the array
+/// expected output: ["Lucycan", "Collin", "Mark", "Cortez", "Blanca"]
